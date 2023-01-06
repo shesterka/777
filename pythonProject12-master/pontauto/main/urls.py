b2d4ef703fcccce05bdde566859ab560220ef0d5
1xbet <1xbetvavada@mail.ru>
2023-01-06 20:38:27 +0300

    obnova10

--- pythonProject12-master/pontauto/main/urls.py
+++ pythonProject12-master/pontauto/main/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('tariff', views.tarif, name='tariff'),
+    path('conditions', views.conditions, name='conditions'),
+    path('rental', views.rental, name='rental'),
+    path('contacts', views.contacts, name='contacts'),
+    path('reviews', views.reviews, name='reviews'),
 ]
 
--- pythonProject12-master/pontauto/main/views.py
+++ pythonProject12-master/pontauto/main/views.py
@@ -9,3 +9,19 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def tarif(request):
+    return render(request, 'main/tariff.html')
+
+def conditions(request):
+    return render(request, 'main/conditions.html')
+
+def rental(request):
+    return render(request, 'main/rental.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+def reviews(request):
+    return render(request, 'main/reviews.html')
