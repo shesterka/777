6771aff893eb9206edc09961c27cd61f9796fbb9
1xbet <1xbetvavada@mail.ru>
2023-01-08 16:29:53 +0300

    obnova15

--- pythonProject12-master/pontauto/news/models.py
+++ pythonProject12-master/pontauto/news/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class artiles(models.Model):
+class Artiles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
@@ -9,3 +9,7 @@ class artiles(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- pythonProject12-master/pontauto/news/views.py
+++ pythonProject12-master/pontauto/news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Artiles
+
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Artiles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
